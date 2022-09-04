14172a878214084ba6aeab9d146206ba89fc143d
Andrew Li <zeyu.li23@gmail.com>
2022-09-04 17:41:38 -0600

    finished first half of dp

---
+++ DP/longest-increasing-subsequence.py
@@ -0,0 +1,11 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        dp = [1 for _ in range(len(nums))]
+        
+        for i in reversed(range(len(nums) - 1)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] < nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+                
+
+        return max(dp)
