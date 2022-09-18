a497a9e3f6439d5f1c01588d0d3e98951f9ac1a4
Andrew Li <zeyu.li23@gmail.com>
2022-09-18 17:18:29 -0600

    updated

--- Binary/sum-of-two-integers.py
+++ Binary/sum-of-two-integers.py
@@ -2,8 +2,21 @@
 class Solution:
     def getSum(self, a: int, b: int) -> int:
         # some abitrary bitwise operation
+        # while b != 0:
+        #     carry = a&b # Carry value is calculated 
+        #     a = a^b # Sum value is calculated and stored in a
+        #     b = carry<<1 # The carry value is shifted towards left by a bit
+        # return a
+
+        # only solution fast enough
+        mask = 0xffffffff
+
         while b != 0:
-            carry = a&b # Carry value is calculated 
-            a = a^b # Sum value is calculated and stored in a
-            b = carry<<1 # The carry value is shifted towards left by a bit
-        return a
+            tmp = (a & b) << 1
+            a = (a ^ b) & mask
+            b = tmp & mask
+
+        if a > mask // 2:
+            return ~(a ^ mask)
+        else:
+            return a
